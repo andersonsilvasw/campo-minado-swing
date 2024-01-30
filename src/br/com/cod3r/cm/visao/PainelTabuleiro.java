3fa641779f98af881ddab28c5a9547856c34954d
andersonsilvasw <andersonsilvaedif@hotmail.com>
2024-01-30 11:15:53 -0300

    Sessão 13 - Aula 10 Interface gráfica do campo minado #02

--- src/br/com/cod3r/cm/visao/PainelTabuleiro.java
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -3,10 +3,16 @@ package br.com.cod3r.cm.visao;
 import br.com.cod3r.cm.modelo.Tabuleiro;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PainelTabuleiro extends JPanel {
 
     public PainelTabuleiro (Tabuleiro tabuleiro) {
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
 
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+        tabuleiro.registrarObservador(e -> {
+            // TODO Mostrar resultado ao usuário
+        });
     }
 }

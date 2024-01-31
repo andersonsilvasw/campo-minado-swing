cd3ad84a2ad5ad76a20ed3d123977d289e74c423
andersonsilvasw <andersonsilvaedif@hotmail.com>
2024-01-30 23:02:55 -0300

    Sessão 13 - Aula 12 Interface gráfica do campo minado #04

--- src/br/com/cod3r/cm/visao/BotaoCampo.java
+++ src/br/com/cod3r/cm/visao/BotaoCampo.java
@@ -6,8 +6,10 @@ import br.com.cod3r.cm.modelo.CampoObservador;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class BotaoCampo extends JButton implements CampoObservador {
+public class BotaoCampo extends JButton implements CampoObservador, MouseListener {
 
     private final Color BG_PADRAO = new Color(184, 184, 184);
     private final Color BG_MARCAR = new Color(8, 179, 247);
@@ -21,6 +23,7 @@ public class BotaoCampo extends JButton implements CampoObservador {
         setBackground(BG_PADRAO);
         setBorder(BorderFactory.createBevelBorder(0));
 
+        addMouseListener(this);
         campo.registrarObservador(this);
     }
 
@@ -51,5 +54,46 @@ public class BotaoCampo extends JButton implements CampoObservador {
     }
 
     private void aplicarEstiloAbrir() {
+        setBackground(BG_PADRAO);
+        setBorder(BorderFactory.createLineBorder(Color.GRAY));
+
+        switch (campo.minasNaVizinhanca()) {
+            case 1:
+                setForeground(TEXTO_VERDE);
+                break;
+            case 2:
+                setForeground(Color.BLUE);
+                break;
+            case 3:
+                setForeground(Color.YELLOW);
+                break;
+            case 4:
+            case 5:
+            case 6:
+                setForeground(Color.RED);
+                break;
+            default:
+                setForeground(Color.PINK);
+        }
+        String valor = !campo.vizinhancaSegura() ? campo.minasNaVizinhanca() + "" : "";
+        setText(valor);
     }
+
+    //Interface dos eventos do mouse
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(e.getButton() == 1) {
+            campo.abrir();
+        } else {
+            campo.alternarMarcacao();
+        }
+    }
+
+    public void mouseClicked(MouseEvent e) {}
+
+    public void mouseReleased(MouseEvent e) {}
+
+    public void mouseEntered(MouseEvent e) {}
+
+    public void mouseExited(MouseEvent e) {}
 }
